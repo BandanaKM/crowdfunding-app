dbea197625067bd412bb148f1df56ec0d1d2360d
BandanaKM <bandanakm@gmail.com>
2015-01-14 16:08:02 -0500

    added header to each page

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,7 +9,7 @@ class Project < ActiveRecord::Base
   # validates :title, presence: true, uniqueness: true
   # validates :creator, presence: true, uniqueness: true
   # validates :image, presence: true
-  # validates :description, presence: true, length: { maximum: 500 }
+  # validates :description, presence: true, length: { maximum: 500000 }
   # validates :total, presence: true, numericality: true
   # validates :goal, presence: true, numericality: true 
 
