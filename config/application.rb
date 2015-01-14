d70410ad1e7579a8fd1749dfa037a60887f71a49
Kelvin Ma <kelvin@codecademy.com>
2015-01-14 13:58:55 -0500

    switched back to rails 3.x

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,5 @@ module CrowdfundingApp
     end
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
